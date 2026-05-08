ac4b4c3f2c4bc1222b385579b1ba03d927936037
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-08 13:33:39 +0900

    ユーザーコントロールをデザイン

---
+++ SampleControl.xaml.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Windows.Controls;
+
+namespace WpfControl.Sample
+{
+
+public partial class SampleControl : UserControl
+{
+
+    //----------------------------------------------------------------
+    /**   デフォルトコンストラクタ
+    **
+    **/
+    public  SampleControl()
+    {
+        InitializeComponent();
+    }
+
+}
+
+}   //  End of namespace  WpfControl.Sample
