aebf63bbbdd71f553f62141d0921b670f3ed1976
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-12 19:16:09 +0900

    インターフェイスを新規作成
    ISampleViewModel

---
+++ ISampleViewModel.cs
@@ -0,0 +1,56 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  WPF UserControl Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+using System.Windows.Input;
+
+namespace WpfControl.Sample
+{
+
+//========================================================================
+//
+//    ISampleViewModel  interface.
+//
+
+public interface ISampleViewModel
+{
+
+    //----------------------------------------------------------------
+    /**   「クリア」ボタン用のコマンドを取得するプロパティ。
+    **
+    **/
+    public  ICommand
+    ClearButtonCommand { get; }
+
+    //----------------------------------------------------------------
+    /**   「実行」ボタン用のコマンドを取得するプロパティ。
+    **
+    **/
+    public  ICommand
+    RunButtonCommand { get; }
+
+    //----------------------------------------------------------------
+    /**   「入力テキスト」プロパティ。
+    **
+    **/
+    public  System.String
+    InputText { get; set; }
+
+    //----------------------------------------------------------------
+    /**   「出力テキスト」プロパティ。
+    **
+    **/
+    public  System.String
+    OutputText { get; set; }
+
+};
