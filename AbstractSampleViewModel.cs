3856b57115652a1a833b7c4246ca52d3634f6c69
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-16 10:35:58 +0900

    抽象クラス AbstractSampleViewModel を新規作成

---
+++ AbstractSampleViewModel.cs
@@ -0,0 +1,30 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  WPF UserControl Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+using System.Windows.Input;
+
+namespace WpfControl.Sample
+{
+
+//========================================================================
+//
+//    AbstractSampleViewModel  class.
+//
+
+public  abstract  class  AbstractSampleViewModel : ISampleViewModel
+{
+
+}   //  End class AbstractSampleViewModel
+
+}   //  End of namespace  WpfControl.Sample
