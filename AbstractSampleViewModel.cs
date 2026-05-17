b2f6a57a24074b5c581bcc97afcb485e2baffb79
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-17 09:34:54 +0900

    警告対応

--- AbstractSampleViewModel.cs
+++ AbstractSampleViewModel.cs
@@ -96,7 +96,7 @@ public abstract class  AbstractSampleViewModel
 //
 
     protected void OnOrpoertyChanged(
-            [CallerMemberName] System.String    propertyName = null)
+            [CallerMemberName]  System.String?  propertyName = null)
     {
         PropertyChanged?.Invoke(
                 this, new PropertyChangedEventArgs(propertyName));
