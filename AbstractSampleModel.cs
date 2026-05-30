15e5ffeb24085eb5c187b400488e91f0eea04509
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-30 13:43:59 +0900

    空のソースファイルを新規作成

---
+++ AbstractSampleModel.cs
@@ -0,0 +1,13 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  WPF UserControl Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
