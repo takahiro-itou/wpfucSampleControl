a353e3da0b3744e82bbf4d5278a298b205bc26c8
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-30 14:34:39 +0900

    ダミー実装

--- AbstractSampleModel.cs
+++ AbstractSampleModel.cs
@@ -11,3 +11,43 @@
 **          or (at your option) any later version.                      **
 **                                                                      **
 *************************************************************************/
+
+
+namespace WpfControl.Sample
+{
+
+//========================================================================
+//
+//    AbstractSampleViewModel  class.
+//
+//    このクラスは別リポジトリ WpfControlLibrary  にある
+//    Common.SimpleCommand  を利用します
+//
+
+public class  AbstractSampleModel
+        : ISampleModel
+{
+
+    //----------------------------------------------------------------
+    /**   クリア動作を実行する。
+    **
+    **/
+    public  void
+    clearTexts()
+    {
+        this.InputText  = "";
+        this.OutputText = "";
+    }
+
+    //----------------------------------------------------------------
+    /**   適当な動作を実行する。
+    **
+    **/
+    public  void
+    executeCommand()
+    {
+    }
+
+}   //  End class AbstractSampleModel
+
+}   //  End of namespace  WpfControl.Sample
